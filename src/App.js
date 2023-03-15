479d1b5eff11deac3cdd00ef4aa39c6cacf5dfa0
Jessica Liu <72271395+crab-milk@users.noreply.github.com>
2023-03-15 12:17:35 -0500

    Added users

--- src/App.js
+++ src/App.js
@@ -1,25 +1,93 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { TextInput } from 'react-native-web';
+import { ref, set, update, onValue, remove } from "firebase/database";
+import { db } from './components/config';
 
 function App() {
+  const [username, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  function createData() {
+    set(ref(db, 'users/' + username), {
+      username: username,
+      email: email
+    }).then(() => {
+      // Data saved successfully!
+      alert('data updated!');
+    })
+      .catch((error) => {
+        // The write failed...
+        alert(error);
+      });
+  }
+
+  function update() {
+    update(ref(db, 'users/' + username), {
+      username: username,
+      email: email
+    }).then(() => {
+      // Data saved successfully!
+      alert('data updated!');
+    })
+      .catch((error) => {
+        // The write failed...
+        alert(error);
+      });
+  };
+
+  function readData() {
+    const starCountRef = ref(db, 'users/' + username);
+    onValue(starCountRef, (snapshot) => {
+      const data = snapshot.val();
+      setEmail(data.email);
+    });
+  }
+
+  function deleteData() {
+    remove(ref(db, 'users/' + username));
+    alert('removed');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <View style={styles.container}>
+        <Text>
+          Firebase crud!
+        </Text>
+
+        <TextInput value={username} onChangeText={(username) => { setName(username) }} placeholder="Username" style={styles.textBoxes}>
+        </TextInput>
+
+        <TextInput value={email} onChangeText={(email) => { setEmail(email) }} placeholder="Email" style={styles.textBoxes}>
+        </TextInput>
+
+        <button onClick={createData}>
+          Submit Data
+        </button>
+
+      </View>
     </div>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textBoxes: {
+    width: '90%',
+    fontSize: 18,
+    padding: 12,
+    borderColor: 'gray',
+    borderWidth: 0.2,
+    borderRadius: 10
+  }
+});
 
 export default App;
