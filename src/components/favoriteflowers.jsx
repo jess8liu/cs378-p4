495cf27e590885ec115f463aecef892948217d27
Jessica Liu <72271395+crab-milk@users.noreply.github.com>
2023-03-15 22:07:03 -0500

    Added mapping for the data on screen

--- src/components/favoriteflowers.jsx
+++ src/components/favoriteflowers.jsx
@@ -1,6 +1,5 @@
 import '../App.css';
 import React, { useState, useEffect } from 'react'
-import { onAuthStateChanged } from 'firebase/auth';
 import { set, ref, onValue } from 'firebase/database';
 import { auth, db } from './config';
 import { uid } from 'uid';
@@ -24,35 +23,26 @@ function FavoriteFlowers() {
 			alert(error);
 		});
 		// Clear flower text and add it to flowerList
+		setFlowerList((oldArray) => [...oldArray, flower]);
 		setFlower('');
-	}
-
-	function update() {
-		update(ref(db, `/${auth.currentUser.uid}/${curr_user}`), {
-			user: curr_user,
-			flowerList: flowerList
-		}).then(() => {
-			// Data saved successfully!
-			alert('Flower list updated!');
-		})
-			.catch((error) => {
-				// The write failed...
-				alert(error);
-			});
-		// Clear flower text and add it to flowerList
-		setFlower('');
-	};
-
-	function readData() {
-		const flowersRef = ref(db, `/${auth.currentUser.uid}/${curr_user}`);
-		onValue(flowersRef, (snapshot) => {
-			const data = snapshot.val();
-			setFlowerList(data.flower);
-		});
 	}
 	
+	// Load list on screen & load from database
 	useEffect(() => {
-
+		auth.onAuthStateChanged(user => {
+			if (user) {
+				onValue(ref(db, `/${auth.currentUser.uid}`), snapshot => {
+					setFlowerList([]);
+					const data = snapshot.val();
+					// Add a new flower
+					if (data != null) {
+						Object.values(data).forEach(flower => {
+							setFlowerList((oldArray) => [...oldArray, flower]);
+						})
+					}
+				})
+			}
+		})
 	}, []);
 
 	return (
@@ -62,9 +52,21 @@ function FavoriteFlowers() {
 			</h2>
 			<div>
 				<div>
-					{flowerList}
-					<input className='FlowerName' type='text' placeholder='A flower on your mind...' value={flower} onChange={(e) => setFlower(e.target.value)}>
-					</input>
+					<div className='FlowerList'>
+						{
+						flowerList.map(flower => (
+							<div>
+								{
+								flower.flower
+								}
+							</div>
+						))
+						}
+					</div>
+					<div>
+						<input className='FlowerName' type='text' placeholder='A flower on your mind...' value={flower} onChange={(e) => setFlower(e.target.value)}>
+						</input>
+					</div>
 				</div>
 
 				<button onClick={save}>
